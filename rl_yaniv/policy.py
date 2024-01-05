acad147b5f2721c39edd35f2ab51fbfb6cadb4b1
Emilio Oldenziel <emilio@oldenziel.net>
2024-01-05 16:51:06 +0100

    rllib marl setup

---
+++ rl_yaniv/policy.py
@@ -0,0 +1,57 @@
+from ray.rllib.policy.policy import Policy
+from ray.rllib.models.modelv2 import _unpack_obs
+import numpy as np
+
+
+class RandomHeuristic(Policy):
+    """Play a random legal move."""
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.exploration = self._create_exploration()
+
+    def compute_actions(
+        self,
+        obs_batch,
+        state_batches=None,
+        prev_action_batch=None,
+        prev_reward_batch=None,
+        info_batch=None,
+        episodes=None,
+        **kwargs
+    ):
+        # actions = []
+        # for x in obs_batch:
+        #     print(x)
+        #     action_mask = x['action_mask']
+        #     valid_action_indices = np.argwhere(action_mask)
+        #     action = np.random.random_sample(valid_action_indices)
+        #     actions.append(action)
+        #
+
+        # Working call to _unpack_obs
+        unpacked_obs = _unpack_obs(
+            np.array(obs_batch, dtype=np.float32), self.observation_space.original_space, tensorlib=np
+        )
+
+        # The nvironment returns observation of the form: {agent_id: {'observations': observations, 'action_mask': action_mask}}
+        action_masks = unpacked_obs["action_mask"]
+
+        actions = []
+        for action_mask in action_masks:
+            if np.any(action_mask):
+                action = np.random.choice(np.argwhere(action_mask).flatten())
+            else:
+                action = 0
+            actions.append(action)
+
+        return actions, [], {}
+
+    def learn_on_batch(self, samples):
+        pass
+
+    def get_weights(self):
+        pass
+
+    def set_weights(self, weights):
+        pass
